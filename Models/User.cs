b90bd8bc47c73a1fa1f3d4727cf6f9cf018bedfb
Gustavo Vicuña G <gpvicuna@miuandes.cl>
2021-10-29 15:05:56 -0300

    API first upload

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoubtedAPI.Models {
+    public class User {
+        public long Id { get; set; }
+        public string Username { get; set; }
+    }
+}
