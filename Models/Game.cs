bf1ee36dc4ef69704c65e3f8bd8772b1205cc951
nccaster <ncapara@miuandes.cl>
2021-11-04 03:40:41 -0300

    password for game cs

--- Models/Game.cs
+++ Models/Game.cs
@@ -12,12 +12,16 @@ namespace DoubtedAPI.Models
         public long Id { get; set; }
         [Required]
         public string GameName { get; set; }
+        [Required]
+        public string PassWord { get; set; }
         public int MaxPlayers { get; set; }
         public int Round { get; set; }
-        public bool NewRound { get; set; }
         public int PlayerTurn { get; set; }
         public bool GameStarted { get; set; }
         public bool Obligando { get; set; }
+        public bool NewRound { get; set; }
+        public bool CanCalzar { get; set; }
+
         [JsonIgnore]
         public List<Player> players { get; set; }
     }
